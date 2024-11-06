01a2db128f1f9beb7b551a644f7776908929e60b
jona-11 <jonathangalimberti@hotmail.com>
2024-11-06 16:14:09 -0300

    modificación en botones sig y ant y filtro por Pokemon

--- dalta.js
+++ dalta.js
@@ -3,48 +3,56 @@ let dataPokemon;
 const infoDevuelta = document.getElementById("info-filtros")
 const seleccion = document.getElementById("seleccion")
 const prueba = document.getElementById("prueba")
-const botonNext = document.getElementById("boton-next")
+const botonNext = document.getElementById("next-previous")
 let paginacion = 0
+const pokemonBusqueda = document.getElementById("pokemon-busqueda")
+const types = document.getElementById("types")
 
-fetch ("https://pokeapi.co/api/v2/type/")
+fetch("https://pokeapi.co/api/v2/type/")
     .then(res => res.json())
-    .then(data => data.results.forEach((item)=>{
+    .then(data => data.results.forEach((item) => {
         return filtros.innerHTML += `<option value="${item.name}">${item.name}</option>`
     }))
 
-    const filtros = document.getElementById("filtros")
+const filtros = document.getElementById("filtros")
 
-    filtros.addEventListener("change", (event) => {
-        cambioFiltro(event.target.value);
-      });
+filtros.addEventListener("change", (event) => {
+    cambioFiltro(event.target.value);
+});
 
-function cambioFiltro(valor){
-    if(valor == ""){
+fetch(baseUrl + '/pokemon?limit=10000')
+    .then(res => res.json())
+    .then(data => data.results.forEach((item) => {return pokemonBusqueda.innerHTML += `<option value="${item.name}">${item.name}</option>` }))
+
+
+function cambioFiltro(valor) {
+    if (valor == "") {
         infoDevuelta.style = "display:none"
         prueba.style = "display:"
         botonNext.style = "display:"
         iniciarPokedex()
     }
-    else{
+    else {
         infoDevuelta.innerHTML = ""
         prueba.style = "display:none"
         botonNext.style = "display:none"
     }
 
-    fetch (baseUrl+`/type/${valor}`)
-    .then(res => res.json())
-    .then(data => data.pokemon.forEach((item) => {return iniciarCarga(item.pokemon.name)}))
+    fetch(baseUrl + `/type/${valor}`)
+        .then(res => res.json())
+        .then(data => data.pokemon.forEach((item) => { return iniciarCarga(item.pokemon.name) }))
 }
 
-
-function iniciarPokedex(){
+function iniciarPokedex() {
 
     fetch(baseUrl + `/pokemon/?limit=20&offset=${paginacion}`)
-    .then(res => res.json())
-    .then(data => { data.results.forEach((item) => {
-        fetch (baseUrl + `/pokemon/${item.name}`)
         .then(res => res.json())
-        .then(data => {; return prueba.innerHTML += `<div class="card col-sm-4 col-md-3 col-lg-2 m-2 bg-body-secondary" id="">
+        .then(data => {
+            prueba.innerHTML = ""; data.results.forEach((item) => {
+                fetch(baseUrl + `/pokemon/${item.name}`)
+                    .then(res => res.json())
+                    .then(data => {
+                         prueba.innerHTML += `<div class="card col-sm-4 col-md-3 col-lg-2 m-2 bg-body-secondary" id="">
             <div id="carouselExample-${item.name}" class="carousel slide">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
@@ -64,32 +72,52 @@ function iniciarPokedex(){
                 </button>
             </div>
                 <div class="card-body">
+                <div>
                     <p class="card-text"><b>${data.name}</b></p>
-                    <p class="card-text">Type: ${data.types[0].type.name}</p> 
+                </div>
+                <div id="types">
+                    <p class="card-text">Type: ${data.types[0].type.name}</p>
+                </div>
                     <h5>Habilidades</h5>
                     <p class="card-text">${data.abilities[0].ability.name}</p>
-                    <p class="card-text">${data.abilities[1].ability.name}</p>
                 </div>
-        </div>
- `
-    })})})
+        </div> `        
+                    })
+            })
+        })
 }
 
-function next(){
-    paginacion = paginacion + 20
-    iniciarPokedex()
+function next(valor) {
+
+    if (valor == "next") {
+        paginacion = paginacion + 20
+        iniciarPokedex()
+    } if (valor == "previous" && paginacion > 0) {
+        paginacion = paginacion - 20
+        iniciarPokedex()
+    }
 }
 
 function seleccionoPokemon() {
-    let seleccion = document.getElementById("seleccion")
-    let pokemonSeleccionado = seleccion.value
+    infoDevuelta.innerHTML = ""
+    let pokemonBusqueda = document.getElementById("pokemon-busqueda")
+    let pokemonSeleccionado = pokemonBusqueda.value
+    if (pokemonSeleccionado == "") {
+        prueba.style = "display:"
+        botonNext.style = "display:"
+    } else {
+        infoDevuelta.innerHTML = ""
+        prueba.style = "display:none"
+        botonNext.style = "display:none"
+    }
     iniciarCarga(pokemonSeleccionado)
 }
 
 function iniciarCarga(pokemonSeleccionado) {
     fetch(baseUrl + `/pokemon/${pokemonSeleccionado}`)
         .then(res => res.json())
-        .then(data => {infoDevuelta.innerHTML += `<div class="card col-sm-4 col-md-3 col-lg-2 m-2 bg-body-secondary" id="">
+        .then(data => {
+            infoDevuelta.innerHTML += `<div class="card col-sm-4 col-md-3 col-lg-2 m-2 bg-body-secondary" id="">
             <div id="carouselExample-${pokemonSeleccionado}" class="carousel slide">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
@@ -115,8 +143,8 @@ function iniciarCarga(pokemonSeleccionado) {
                     <p class="card-text">${data.abilities[0].ability.name}</p>
                     <p class="card-text">${data.abilities[1].ability.name}</p>
                 </div>
-        </div> `; 
-})
+        </div> `;
+        })
 }
 
 iniciarPokedex()
